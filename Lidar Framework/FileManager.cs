6754964e58afce66a8d7b61828d14f3cda46c21a
Даниил Клименков <klimenkov2006@outlook.com>
2024-06-08 14:26:41 +0300

    Добавьте файлы проекта.

---
+++ Lidar Framework/FileManager.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using Newtonsoft.Json;
+namespace Lidar_Framework
+{
+   public class FileManager
+   {
+      private const string RecentFilesPath = "D:\\OneDrive\\Курсач\\Проект\\Lidar Framework\\Lidar Framework\\recentFiles.json";
+      public List<Point3D> points { get; private set; } = new List<Point3D>();
+      public List<string> RecentLoadFiles { get; private set; } = new List<string>();
+      public List<string> RecentSaveFiles { get; private set; } = new List<string>();
+      private const int MaxRecentFilesCount = 5;
+      public FileManager()
+      {
+         LoadRecentFiles();
+      }
+      public async Task LoadPCDAsync(string filePath)
+      {
+         points.Clear();
+         if (!File.Exists(filePath))
+         {
+            throw new FileNotFoundException($"File {filePath} not found.");
+         }
+
+         string[] lines = await Task.Run(() => File.ReadAllLines(filePath));
+
+         var newPoints = new List<Point3D>();
+         for (int i = 11; i < lines.Length; i++)
+         {
+            string[] values = lines[i].Split(' ');
+            if (values.Length < 3) continue;
+
+            if (double.TryParse(values[0], NumberStyles.Float, CultureInfo.InvariantCulture, out double x) &&
+                double.TryParse(values[1], NumberStyles.Float, CultureInfo.InvariantCulture, out double y) &&
+                double.TryParse(values[2], NumberStyles.Float, CultureInfo.InvariantCulture, out double z))
+            {
+               newPoints.Add(new Point3D(x, y, z));
+            }
+         }
+
+         // Операция присвоения выполнится быстро, так как points уже очищен
+         points.AddRange(newPoints);
+      }
+      public async Task SavePCDAsync(string filePath)
+      {
+         using (StreamWriter writer = new StreamWriter(filePath))
+         {
+            // Add headers if needed
+            await writer.WriteLineAsync("# .PCD v0.7 - Point Cloud Data file format");
+            await writer.WriteLineAsync("VERSION 0.7");
+            await writer.WriteLineAsync("FIELDS x y z");
+            await writer.WriteLineAsync("SIZE 4 4 4");
+            await writer.WriteLineAsync("TYPE F F F");
+            await writer.WriteLineAsync("COUNT 1 1 1");
+            await writer.WriteLineAsync("WIDTH " + points.Count);
+            await writer.WriteLineAsync("HEIGHT 1");
+            await writer.WriteLineAsync("VIEWPOINT 0 0 0 1 0 0 0");
+            await writer.WriteLineAsync("POINTS " + points.Count);
+            await writer.WriteLineAsync("DATA ascii");
+
+            foreach (var point in points)
+            {
+               await writer.WriteLineAsync($"{point.X.ToString(CultureInfo.InvariantCulture)} {point.Y.ToString(CultureInfo.InvariantCulture)} {point.Z.ToString(CultureInfo.InvariantCulture)}");
+            }
+         }
+      }
+      private void LoadRecentFiles()
+      {
+         if (File.Exists(RecentFilesPath))
+         {
+            string json = File.ReadAllText(RecentFilesPath);
+            var recentFiles = JsonConvert.DeserializeObject<RecentFiles>(json);
+            if (recentFiles != null)
+            {
+               RecentLoadFiles = recentFiles.Load ?? new List<string>();
+               RecentSaveFiles = recentFiles.Save ?? new List<string>();
+            }
+         }
+      }
+      public class RecentFiles
+      {
+         public List<string> Load { get; set; }
+         public List<string> Save { get; set; }
+      }
+      private void SaveRecentFiles()
+      {
+         var recentFiles = new Dictionary<string, List<string>>
+        {
+            { "Load", RecentLoadFiles },
+            { "Save", RecentSaveFiles }
+        };
+         string json = JsonConvert.SerializeObject(recentFiles);
+         File.WriteAllText(RecentFilesPath, json);
+      }
+      public void AddToRecentFiles(List<string> recentFiles, string filePath)
+      {
+         // Приводим filePath к нижнему регистру для регистронезависимого сравнения
+         string lowerCaseFilePath = filePath.ToLower();
+
+         // Проверяем наличие файла в списке, используя регистронезависимое сравнение
+         if (!recentFiles.Any(f => f.ToLower() == lowerCaseFilePath))
+         {
+            recentFiles.Insert(0, filePath);
+            if (recentFiles.Count > MaxRecentFilesCount)
+            {
+               recentFiles.RemoveAt(recentFiles.Count - 1);
+            }
+            SaveRecentFiles();
+         }
+      }
+      public string GetFormattedFileSize(long fileSizeInBytes)
+      {
+         double fileSize = 0;
+         string fileSizeText = "";
+
+         if (fileSizeInBytes < 1024)
+         {
+            fileSize = fileSizeInBytes;
+            fileSizeText = " Б";
+         }
+         else if (fileSizeInBytes < 1024 * 1024)
+         {
+            fileSize = (double)fileSizeInBytes / 1024;
+            fileSizeText = " КБ";
+         }
+         else if (fileSizeInBytes < 1024 * 1024 * 1024)
+         {
+            fileSize = (double)fileSizeInBytes / (1024 * 1024);
+            fileSizeText = " МБ";
+         }
+         else
+         {
+            fileSize = (double)fileSizeInBytes / (1024 * 1024 * 1024);
+            fileSizeText = " ГБ";
+         }
+
+         return fileSize.ToString("0.##") + fileSizeText;
+      }
+
+   }
+}
---
+++ Lidar Framework/MainWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using HelixToolkit.Wpf;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using plotLib = ScottPlot; // Установка псевдонима для ScottPlot
+using System.IO.Ports;
+
+
+namespace Lidar_Framework
+{
+   /// <summary>
+   /// Логика взаимодействия для MainWindow.xaml
+   /// </summary>
+
+   public partial class MainWindow : Window
+   {
+      private FileManager fileManager = new FileManager();
+
+      public MainWindow()
+      {
+         InitializeComponent();
+         DirectionalLight directionalLight = new DirectionalLight(Colors.White, new Vector3D(0, 0, -1));
+         ModelVisual3D lightModel = new ModelVisual3D();
+         lightModel.Content = directionalLight;
+         viewport3D.Children.Add(lightModel);
+
+         UpdateRecentFilesListBox(lbLastLoad, fileManager.RecentLoadFiles);
+         UpdateRecentFilesListBox(lbLastSave, fileManager.RecentSaveFiles);
+
+      }
+
+      private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+      {
+         // Создание коллекции точек
+         var points = new Point3DCollection();
+         Random random = new Random();
+         for (int i = 0; i < 255000; i++)
+         {
+            // Генерация случайных координат точек в пределах [-10, 10]
+            double x = random.NextDouble() * 20 - 1;
+            double y = random.NextDouble() * 20 - 1;
+            double z = random.NextDouble() * 20 - 1;
+
+            points.Add(new Point3D(x, y, z));
+         }
+
+         // Создание модели точек
+         var visual = new ModelVisual3D();
+
+         var pointsVisual = new PointsVisual3D
+         {
+            Points = points,
+            Size = 3,
+            Color = Colors.Red
+         };
+
+         visual.Children.Add(pointsVisual);
+
+         viewport3D.Children.Add(visual);
+      }
+
+      private void Points_2D()
+      {
+         // Преобразование точек в массивы для ScottPlot
+         double[] xs = fileManager.points.Select(p => p.X).ToArray();
+         double[] ys = fileManager.points.Select(p => p.Y).ToArray();
+
+         // Построение графика
+         var plt = wpfPlot.Plot;
+         plt.Clear();
+         plt.Add.ScatterPoints(xs, ys, color: plotLib.Color.FromColor(System.Drawing.Color.FromArgb(40, 144, 255))); // Используем метод Add.Scatter
+         plt.Axes.SquareUnits();
+
+         plt.Axes.Margins(left: 0.1, right: 0.9, top: 0.9, bottom: 0.1);
+         plt.Layout.Frameless();
+         // Установка квадратных единиц измерения
+         plt.Axes.SquareUnits();
+
+         wpfPlot.Refresh();
+      }
+
+      private void Points_3D()
+      {
+         viewport3D.Children.Clear();
+
+         var pointsVisual = new PointsVisual3D
+         {
+            Points = new Point3DCollection(fileManager.points),
+            Size = 4,
+            Color = Colors.DodgerBlue
+         };
+
+         viewport3D.Children.Add(pointsVisual);
+      }
+
+      private void btnSave_Click(object sender, RoutedEventArgs e)
+      {
+         SaveFileDialog saveFileDialog = new SaveFileDialog();
+         saveFileDialog.Filter = "PCD files (*.pcd)|*.pcd|All files (*.*)|*.*";
+
+         bool? result = saveFileDialog.ShowDialog();
+
+         if (result == true)
+         {
+            string filePath = saveFileDialog.FileName;
+            try
+            {
+               fileManager.SavePCDAsync(filePath);
+               fileManager.AddToRecentFiles(fileManager.RecentSaveFiles, filePath);
+               UpdateRecentFilesListBox(lbLastSave, fileManager.RecentSaveFiles);
+               MessageBox.Show("Файл успешно сохранен.");
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message);
+            }
+         }
+      }
+
+
+
+      private async void btnLoad_Click(object sender, RoutedEventArgs e)
+      {
+         OpenFileDialog openFileDialog = new OpenFileDialog();
+         openFileDialog.Filter = "PCD files (*.pcd)|*.pcd|All files (*.*)|*.*";
+
+         // Открытие диалогового окна в UI-потоке
+         bool? result = openFileDialog.ShowDialog();
+
+         if (result == true)
+         {
+            string fullPath = openFileDialog.FileName;
+            string fileName = Path.GetFileName(fullPath);
+            string filePath = Path.GetDirectoryName(fullPath);
+
+            try
+            {
+               // Загрузка выбранного файла и обновление интерфейса
+               await LoadPointAsync(fileName, filePath, fullPath);
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message);
+            }
+         }
+      }
+
+
+      private async Task LoadPointAsync(string fileName, string filePath, string fullPath)
+      {
+         UpdateFileInfoExpander(fullPath); // Обновление информации о файле в Expander
+         await fileManager.LoadPCDAsync(fullPath); // Загрузка выбранного файла
+         fileManager.AddToRecentFiles(fileManager.RecentLoadFiles, fullPath); // Добавление файла в список последних загруженных файлов
+         UpdateRecentFilesListBox(lbLastLoad, fileManager.RecentLoadFiles); // Обновление списка последних загруженных файлов в интерфейсе
+         Points_2D(); // Обновление 2D графика
+         Points_3D(); // Обновление 3D графика
+      }
+
+      private void UpdateFileInfoExpander(string fullPath)
+      {
+         FileInfo fileInfo = new FileInfo(fullPath);
+         // Обновление имени файла
+         FileNameTextBlock.Text = "Имя файла: " + Path.GetFileNameWithoutExtension(fullPath);
+         // Обновление пути файла
+         FilePathTextBlock.Text = "Путь: " + Path.GetDirectoryName(fullPath);
+         // Обновление расширения файла
+         FileExtensionTextBlock.Text = "Расширение: " + Path.GetExtension(fullPath);
+         // Получение размера файла в байтах
+         long fileSizeInBytes = fileInfo.Length;
+         // Получение форматированного размера файла
+         string formattedFileSize = fileManager.GetFormattedFileSize(fileSizeInBytes);
+         // Обновление размера файла
+         FileWeightTextBlock.Text = "Размер: " + formattedFileSize;
+      }
+
+
+
+      private async void UpdateRecentFilesListBox(ListBox listBox, List<string> recentFiles)
+      {
+         listBox.Items.Clear();
+         foreach (string file in recentFiles)
+         {
+            string fileName = Path.GetFileName(file);
+            string path = Path.GetDirectoryName(file).ToLower();
+
+            listBox.Items.Add(new { FileName = fileName, Path = path});
+         }
+      }
+
+      private async void lbLastLoad_SelectionChanged(object sender, SelectionChangedEventArgs e)
+      {
+         ListBox listBox = sender as ListBox;
+         if (listBox.SelectedItem != null)
+         {
+            var selectedItem = listBox.SelectedItem as dynamic;
+            string filePath = selectedItem.Path;
+            string fileName = selectedItem.FileName;
+            string fullPath = Path.Combine(filePath, fileName);
+            if (!File.Exists(fullPath))
+            {
+               MessageBox.Show($"Файл {fullPath} не найден.", "Файл отсутствует", MessageBoxButton.OK);
+            }
+            else
+            {
+               // Загрузка выбранного файла и обновление интерфейса
+               await LoadPointAsync(fileName, filePath, fullPath);
+            }
+         }
+      }
+
+
+
+   }
+}
+
+
+
+
+
